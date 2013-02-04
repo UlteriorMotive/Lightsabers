b549299ed00ba5e38b63d7eab23067fb566a3ba4
Greg Latham <sdge98@gmail.com>
2013-02-03 17:47:50 -0800

    Removed old src

--- src/lightcraft/core/LightToolMaterial.java
+++
@@ -1,9 +0,0 @@
-package ulterior.lightcraft.core;
-
-import net.minecraft.item.EnumToolMaterial;
-import net.minecraftforge.common.EnumHelper;
-
-public class LightToolMaterial {
-
-
-}
--- src/lightcraft/lib/References.java
+++
@@ -1,7 +0,0 @@
-package ulterior.lightcraft.lib;
-
-public class References {
-
-	public static final String COMPROXY = "ulterior.lightcraft.core.CommonProxy";
-	public static final String CLTPROXY = "ulterior.lightcraft.core.ClientProxy";
-}
